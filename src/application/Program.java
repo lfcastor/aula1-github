6c3056939e4373ef98e1a65a2049c2444542cb8d
lfcastor <lfcastor@yahoo.com.br>
2019-09-30 21:09:05 -0300

    acresentada a mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("bom dia");
-
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 	}
-
 }
